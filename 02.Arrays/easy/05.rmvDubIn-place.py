28ba200781810217ad8ef06d98932db51c4a4b85
Vijay <vbvijay84@gamil.com>
2025-06-01 20:25:43 +0530

    b

--- 02.Arrays/easy/05.rmvDubIn-place.py
+++ 02.Arrays/easy/05.rmvDubIn-place.py
@@ -2,12 +2,11 @@ def rmvDubIn_place(nums):
     i = 0
     for j in range(1, len(nums)):
         if nums[i] != nums[j]:
-            i+=1
+            i += 1
             nums[i] = nums[j]
-    return i+1
+    return i + 1, nums[: i + 1]
 
 
-
-nums = [1,1,2,2,2,3,3]
+nums = [1, 1, 2, 2, 2, 3, 3]
 res = rmvDubIn_place(nums)
 print(res)
